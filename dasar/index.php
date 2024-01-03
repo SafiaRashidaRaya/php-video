43a5ea66b2bfeb8ffed82b1d48c1d18988327d56
SafiaRashidaRaya <140325055+SafiaRashidaRaya@users.noreply.github.com>
2024-01-03 13:37:18 +0700

    versi 1.1
    Membuat Tampilan

---
+++ dasar/index.php
@@ -0,0 +1,20 @@
+<?php 
+    echo "belajar";
+    echo "<br>";
+    echo 1000;
+    echo "<br>";
+
+    $hruf = "saya belajar";
+
+    // echo $hruf;
+?>
+
+<?php 
+    echo "<h1>".$hruf."</h1>";
+    echo "<br>";
+    echo '<h3>$hruf</h3>';
+
+    echo "<br>"
+?>
+
+<h2><?=$hruf?></h2>
